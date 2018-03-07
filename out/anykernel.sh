7732afd7c990ab1e4a4fa1777b3e9fbbb9c93f0d
Ram Rasekar <ram.rasekar946@gmail.com>
2018-03-07 15:56:58 +0530

    Automation

---
+++ out/anykernel.sh
@@ -0,0 +1,173 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# EDIFY properties
+
+do.devicecheck=1
+do.initd=1
+do.modules=0
+do.cleanup=1
+device.name1=Z00A
+device.name2=Z008
+device.name3=
+device.name4=
+device.name5=
+
+# shell variables
+block=/dev/block/by-name/boot;
+
+## end setup
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# set up extracted files and directories
+ramdisk=/tmp/anykernel/ramdisk;
+bin=/tmp/anykernel/tools;
+split_img=/tmp/anykernel/split_img;
+patch=/tmp/anykernel/patch;
+
+chmod -R 755 $bin;
+mkdir -p $ramdisk $split_img;
+
+OUTFD=/proc/self/fd/$1;
+
+# ui_print <text>
+ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
+
+# contains <string> <substring>
+contains() { test "${1#*$2}" != "$1" && return 0 || return 1; }
+
+# dump boot and extract ramdisk
+dump_boot() {
+  dd if=$block of=/tmp/anykernel/boot.img;
+  cd $split_img;
+  $bin/unmkbootimg /tmp/anykernel/boot.img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Dumping/splitting image failed. Aborting..."; exit 1;
+  fi;
+  mv -f $ramdisk /tmp/anykernel/rdtmp;
+  mkdir -p $ramdisk;
+  cd $ramdisk;
+  gunzip -c $split_img/initramfs.cpio.gz | cpio -i;
+  cp -af /tmp/anykernel/rdtmp/* $ramdisk;
+}
+
+# repack ramdisk then build and write image
+write_boot() {
+  cd $ramdisk;
+  find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
+  cd /tmp/anykernel;
+  $bin/mkbootimg --kernel /tmp/anykernel/zImage --ramdisk /tmp/anykernel/ramdisk-new.cpio.gz --second $split_img/second.gz --base 0x10000000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100 --cmdline 'init=/init pci=noearly console=logk0 loglevel=0 vmalloc=256M androidboot.hardware=mofd_v1 watchdog.watchdog_thresh=60 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 gpt snd_pcm.maximum_substreams=8 ptrace.ptrace_can_access=1 panic=15 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on debug_locks=0 n_gsm.mux_base_conf="ttyACM0,0 ttyXMM0,1" bootboost=1' -o boot-new.img;
+  mv /tmp/anykernel/boot-new.img /tmp/anykernel/boot-new2.img;
+  cat /tmp/anykernel/boot-new2.img /tmp/anykernel/boot.sig > /tmp/anykernel/boot-new.img;
+  dd if=/tmp/anykernel/boot-new.img of=$block;
+}
+
+# backup_file <file>
+backup_file() { cp $1 $1~; }
+
+# replace_string <file> <if search string> <original string> <replacement string>
+replace_string() {
+  if [ -z "$(grep "$2" $1)" ]; then
+      sed -i "s;${3};${4};" $1;
+  fi;
+}
+
+# replace_section <file> <begin search string> <end search string> <replacement string>
+replace_section() {
+  line=`grep -n "$2" $1 | head -n1 | cut -d: -f1`;
+  sed -i "/${2//\//\\/}/,/${3//\//\\/}/d" $1;
+  sed -i "${line}s;^;${4}\n;" $1;
+}
+
+# remove_section <file> <begin search string> <end search string>
+remove_section() {
+  sed -i "/${2//\//\\/}/,/${3//\//\\/}/d" $1;
+}
+
+# insert_line <file> <if search string> <before|after> <line match string> <inserted line>
+insert_line() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | head -n1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;${5}\n;" $1;
+  fi;
+}
+
+# replace_line <file> <line replace string> <replacement line>
+replace_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | head -n1 | cut -d: -f1`;
+    sed -i "${line}s;.*;${3};" $1;
+  fi;
+}
+
+# remove_line <file> <line match string>
+remove_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | head -n1 | cut -d: -f1`;
+    sed -i "${line}d" $1;
+  fi;
+}
+
+# prepend_file <file> <if search string> <patch file>
+prepend_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo "$(cat $patch/$3 $1)" > $1;
+  fi;
+}
+
+# insert_file <file> <if search string> <before|after> <line match string> <patch file>
+insert_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | head -n1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;\n;" $1;
+    sed -i "$((line - 1))r $patch/$5" $1;
+  fi;
+}
+
+# append_file <file> <if search string> <patch file>
+append_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo -ne "\n" >> $1;
+    cat $patch/$3 >> $1;
+    echo -ne "\n" >> $1;
+  fi;
+}
+
+# replace_file <file> <permissions> <patch file>
+replace_file() {
+  cp -pf $patch/$3 $1;
+  chmod $2 $1;
+}
+
+## end methods
+
+
+## AnyKernel permissions
+# set permissions for included files
+chmod -R 755 $ramdisk;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+rm ./lib/modules/*.ko;
+cp -af /tmp/anykernel/modules/*.ko ./lib/modules/;
+chmod 0644 ./lib/modules/*.ko;
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
